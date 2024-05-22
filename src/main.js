cd662a5d193a9f4c56ea8bd4555e35b629999095
nulldel <17611756357@163.com>
2024-05-22 17:16:07 +0800

    测试有没有空格

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import store from './store'
-import router from './router'
-// 导入自己的UI组件库
-import UI from '@/components/library'
+            import { createApp } from 'vue'
+            import App from './App.vue'
+            import store from './store'
+            import router from './router'
+            // 导入自己的UI组件库
+            import UI from '@/components/library'
 
-// 1,重置样式的库
-import 'normalize.css'
-// 2,自己项目的重置样式金额公共样式
-import '@/assets/styles/common.less'
-// 3.样式变量
-import '@/assets/styles/variables.less'
+            // 1,重置样式的库
+            import 'normalize.css'
+            // 2,自己项目的重置样式金额公共样式
+            import '@/assets/styles/common.less'
+            // 3.样式变量
+            import '@/assets/styles/variables.less'
 
-createApp(App).use(store).use(router).use(UI).mount('#app')
+            createApp(App).use(store).use(router).use(UI).mount('#app')
+// 测试有没有空格
