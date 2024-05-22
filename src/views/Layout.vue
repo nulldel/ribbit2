7619f4082294e96d2abeece42c1a95f9de4e1d4b
nulldel <17611756357@163.com>
2024-05-22 17:00:40 +0800

    测试空格

--- src/views/Layout.vue
+++ src/views/Layout.vue
@@ -1,50 +1,48 @@
-<template>
-  <!-- 顶部通栏 -->
-  <AppNavbar/>
+                                <template>
+            <!-- 顶部通栏 -->
+            <AppNavbar />
 
-  <!-- 吸顶头部组件 -->
-  <!-- <appHeaderStickyVue></appHeaderStickyVue> -->
+            <!-- 吸顶头部组件 -->
+            <!-- <appHeaderStickyVue></appHeaderStickyVue> -->
 
-  <!-- 头部组件 -->
-  <appHeaderVue></appHeaderVue>
+            <!-- 头部组件 -->
+            <appHeaderVue> </appHeaderVue>
 
-  <!-- 内容容器 -->
-  <div class="main">
-    <!-- 二级路由 -->
-    <RouterView/>
-  </div>
+            <!-- 内容容器 -->
+            <div class="main">
+              <!-- 二级路由 -->
+              <RouterView />
+            </div>
 
-  <!-- 底部组件 -->
-  <appFooterVue></appFooterVue>
+            <!-- 底部组件 -->
+            <appFooterVue></appFooterVue>
+          </template>
 
-</template>
+                                <script>
+          import AppNavbar from "@/components/app-navbar.vue";
+          import appHeaderVue from "@/components/app-header.vue";
+          import appFooterVue from "@/components/app-footer.vue";
+          import appHeaderStickyVue from "@/components/app-header-sticky.vue";
+          import { useStore } from "vuex";
+          export default {
+            name: "Lay-out",
+            components: {
+              AppNavbar,
+              appHeaderVue,
+              appFooterVue,
+              appHeaderStickyVue,
+            },
+            // 获取分类数据
+            setup() {
+              // console.log(useStore())
+              const store = useStore();
+              store.dispatch("category/getList");
+            },
+          };
+          </script>
 
-<script>
-import AppNavbar from '@/components/app-navbar.vue'
-import appHeaderVue from '@/components/app-header.vue'
-import appFooterVue from '@/components/app-footer.vue'
-import appHeaderStickyVue from '@/components/app-header-sticky.vue'
-import { useStore } from 'vuex'
-export default {
-  name: 'Lay-out',
-  components: {
-    AppNavbar,
-    appHeaderVue,
-    appFooterVue,
-    appHeaderStickyVue
-  },
-  // 获取分类数据
-  setup () {
-    // console.log(useStore())
-    const store = useStore()
-    store.dispatch('category/getList')
-  }
-
-}
-</script>
-
-<style lang="less" scoped>
-.main{
-  height: 100%;
-}
-</style>
+                                <style lang="less" scoped>
+          .main {
+            height: 100%;
+          }
+          </style>
