3fd5d1a0628bc6a87cf2ad785cb17f0262c9858a
nulldel <17611756357@163.com>
2024-06-07 10:48:14 +0800

    测试

--- src/views/login/components/login-form.vue
+++ src/views/login/components/login-form.vue
@@ -2,13 +2,14 @@
   <div class="account-box">
     <div class="toggle">
       <a @click="isMsgLogin = false" href="javascript:;" v-if="isMsgLogin">
-        <i class="iconfont icon-user"></i> 使用账号登录 手机号
+        <i class="iconfont icon-user"></i> 使用账号登录 手机号手机号
       </a>
       <a @click="isMsgLogin = true" href="javascript:;" v-else>
-        <i class="iconfont icon-msg"></i> 使用短信登录 手机号
+        <i class="iconfont icon-msg"></i> 使用短信登录 手机号手机号
       </a>
     </div>
     <div class="form">
+      <!-- 你在那 -->
       <Form
         class="form"
         :validation-schema="mySchema"
